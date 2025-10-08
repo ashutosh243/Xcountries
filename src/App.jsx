79b5941afd4fc43414e7eb7a3d69e217c069f814
ashutoshgupt243 <vnsashutoshit.gmail.com>
2025-10-08 11:32:29 +0530

    project done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Countries from './Countries';
+
+const App=()=> {
+  return (
+
+    <Countries></Countries>
+  )
+}
+
+export default App
---
+++ src/Countries.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Style from './countries.module.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const Countries=()=> {
+
+  const [country,setCountry]=useState([]);
+  useEffect(()=>{
+           async function getCountry(){
+              let response=await axios.get('https://xcountries-backend.labs.crio.do/all');
+              setCountry(()=>response.data);
+
+           }
+           getCountry();
+  },[]);
+  return (
+    
+    <div className={Style.container}>
+       {
+         
+         country.map((data,index)=>{ return <div key={index} className={Style.country}><img src={data.flag} alt={data.name}/><p>{data.name}</p></div>})
+       }
+    </div>
+  )
+}
+
+export default Countries;
